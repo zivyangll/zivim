6362565522d3c05bb17f889128bb4e8a1411ac91
zivyangll <zivyangll@gmail.com>
2019-09-13 12:47:36 +0800

    feat: use link file

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 echo Install Starting …
 rm -rf ~/.vim* ~/.vimrc
 git clone https://github.com/yllziv/zivim.git ~/.vim --depth=1
-ln -s ~/.vim/.vimrc ~/.vimrc
+script_dir="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd  )"
+ln -sf "$script_dir/.vimrc" ~/.vimrc
 mkdir ~/.vim/undo
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall! +qall
